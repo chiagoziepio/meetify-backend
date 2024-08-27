3d29859583c895cb5f46a05fafae2bd129bba309
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-08-27 12:37:49 +0100

    users and posts model done

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const cookieparser = require("cookie-parser")
+const dotenv = require("dotenv").config()
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://0.0.0.0/meetifydb")
+const connc = mongoose.connection
+connc.once('open',()=>{
+    console.log('connected to database');
+    app.listen(PORT, ()=>{
+        console.log(`server runing on port: ${PORT}`);
+        
+    })
+});
+connc.on('error',(err)=>{
+    console.log(`database error:${err}`);
+    process.exit()
+})
---
+++ model/schema.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true,
+        
+    },
+    profilePic: {
+        type: String,
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }],
+    LikedPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }],
+})
+
+const commentSchema = mongoose.Schema({
+    commentedBy:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default : Date.now
+    }
+})
+
+const likesSchema = mongoose.Schema({
+    likedBy:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    count: {
+        type: Number,
+        default: 0
+    }
+})
+const postSchema = mongoose.Schema({
+    authorName :{
+        type: String,
+        required: true
+    },
+    authorProfilePic: {
+        type: String,
+    },
+    content:{
+        type: String
+    },
+    postImage:{
+        type: String
+    },
+    comment: [commentSchema],
+    likes: [likesSchema]
+})
+
+const UserModel = mongoose.model("User", userSchema);
+const PostModel = mongoose.model("Post", postSchema)
+
+module.exports = {UserModel, PostModel}
