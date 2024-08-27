ffb1bebebb92953af0a5f742fb97a545434c715f
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-08-27 13:37:38 +0100

    register user logic

--- controllers/userController/userController.js
+++ controllers/userController/userController.js
@@ -2,7 +2,30 @@ const bcrypt =  require("bcryptjs")
 const {UserModel} =  require("../../model/schema")
 
 const handleRegisterUser = (async(req,res)=>{
-    res.sendStatus(200)
+    const {fullname,password,username,email,phone_number} = req.body
+    if(!fullname || !password || !username || !email || !phone_number) return res.status(400).json({status: "failed", msg:"Fill all blank"});
+    
+    try {
+        const isUserAlreadyExisting = await UserModel.findOne({email});
+        if(isUserAlreadyExisting) return res.status(400).json({status: "failed", msg:"user already existed"})
+         
+        const hashPwd = await bcrypt.hash(password, 10);
+        const newUser =  new UserModel({
+            fullname,
+            username,
+            email,
+            phone: +phone_number,
+            password: hashPwd
+        });
+        newUser.save()
+    
+        
+        return res.status(201).json({status: "success", msg:"User created" })
+    } catch (error) {
+        console.log(error);
+       return res.status(500).json({status: "failed", msg: `server error: ${error}`})
+    
+    }
 })
 
 module.exports = {handleRegisterUser}
