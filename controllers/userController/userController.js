5f5514ce2c9bc8ec48d606445364cae989565267
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-09-04 21:26:56 +0100

    adding friends setup

--- controllers/userController/userController.js
+++ controllers/userController/userController.js
@@ -7,6 +7,7 @@ const { v2: uuidv2 } = require('uuid'); // For generating unique filenames
 const streamifier = require('streamifier'); // Convert buffer to stream
 const {handleTokenVerification} = require("../../middleware/verifyJwt")
 const fs = require('fs');
+const { log } = require("console")
 const handleRegisterUser = (async(req,res)=>{
     const {fullname,password,username,email,phone_number} = req.body
     if(!fullname || !password || !username || !email || !phone_number) return res.status(400).json({status: "failed", msg:"Fill all blank"});
@@ -177,4 +178,42 @@ const handleGetAllusers = (async(req,res)=>{
         return res.status(500).json({status: "failed", msg: error})
     }
 })
-module.exports = {handleRegisterUser,handleUserLogin,handleUserProfilePicUpload, handleUserBackgroundPIcChange,handleUserLogout, handleGetAllusers}
+
+const handleAddFriends = (async(req,res)=>{
+    const authHeader = req.headers.authorization;
+    const token = authHeader ? authHeader.split(' ')[1] : null;
+    const {id} = req.body
+    try {
+
+        if(!token) return res.status(401).json({status: "failed", msg: "access denied"})
+            const decoded = jwt.verify(token, process.env.REFRESHTOKEN_SECRET_KEY)
+            if(!decoded) return res.status(401).json({status: "failed", msg: "invalid token"})
+            const email =  decoded.email;
+            const LoggedInUser = await UserModel.findOne({email})
+            const findUser = await UserModel.findById(id)
+             if(!findUser) return res.status(400).json({status: "failed", msg : "not a registered user id"})
+
+            if(LoggedInUser.friends.some(id => id.equals(findUser._id) ) || findUser.friends.some(id => id.equals(LoggedInUser._id) ) ){
+                return res.status(400).json({status: "failed", msg: "already friend"})
+            }  
+            
+            await  LoggedInUser.friends.push(findUser._id)
+            await findUser.friends.push(LoggedInUser._id)
+
+            await LoggedInUser.save()
+            await findUser.save()
+            await UserModel.updateOne(
+                {email: email},
+                { $set: {
+                    lastActivity: Date.now(), online: true
+                }
+
+                }
+            )
+        const updatedUser = await UserModel.findOne({email})
+        return res.status(201).json({status: "success", msg: "Added to friends list", user: updatedUser})
+    } catch (error) {
+        return res.status(500).json({status: "failed", msg: error})
+    }
+})
+module.exports = {handleRegisterUser,handleUserLogin,handleUserProfilePicUpload, handleUserBackgroundPIcChange,handleUserLogout, handleGetAllusers, handleAddFriends}
--- routes/userRoute/users.js
+++ routes/userRoute/users.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {handleRegisterUser, handleUserLogin,handleUserProfilePicUpload,handleUserBackgroundPIcChange, handleUserLogout,handleGetAllusers } = require("../../controllers/userController/userController")
+const {handleRegisterUser, handleUserLogin,handleUserProfilePicUpload,handleUserBackgroundPIcChange, handleUserLogout,handleGetAllusers, handleAddFriends } = require("../../controllers/userController/userController")
 const upload = require("../../config/multer")
 
 
@@ -9,5 +9,6 @@ router.post("/login", handleUserLogin)
 router.post("/profilepicupload",upload.single('image'), handleUserProfilePicUpload)
 router.post("/backgroundpicupload",upload.single('image'),handleUserBackgroundPIcChange )
 router.post("/logout",handleUserLogout)
+router.post("/addfriend", handleAddFriends)
 router.get("/getallUser", handleGetAllusers)
 module.exports = router
